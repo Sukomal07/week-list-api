631b7dc4c3139d7a4830aaf9272b6e030c9843e0
Sukomal07 <hypersukomal@gmail.com>
2023-12-03 21:55:25 +0530

    create weeklist model

---
+++ models/weeklist.model.js
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+
+const taskSchema = new Schema({
+    task: {
+        type: String,
+        required: [true, "Task description is required"]
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    },
+    completedTime: {
+        type: Date
+    }
+});
+
+const weeklistSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    name: {
+        type: String,
+        required: [true, "Weeklist name is required"],
+        minLength: [5, "Weeklist name must be at least 5 characters"]
+    },
+    description: [taskSchema],
+    endTime: {
+        type: Date
+    },
+    state: {
+        type: String,
+        enum: ['active', 'inactive', 'completed'],
+        default: 'active'
+    }
+}, { timestamps: true });
+
+const Weeklist = model('Weeklist', weeklistSchema);
+
+export default Weeklist;
